842021a497872d4a7251069b08b41f4629ebb587
jizero4101@naver.com <jizero4101@naver.com>
2025-02-02 18:29:08 +0900

    이중스플래시문제 해결

--- android/app/src/main/java/com/jizerotodo/MainActivity.kt
+++ android/app/src/main/java/com/jizerotodo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.jizerotodo
 
-import android.os.Bundle
-//import androidx.core.splashscreen.SplashScreen
-//import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.os.Build
 
+import android.os.Bundle
+
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,27 +13,30 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "todo"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "todo"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-    
-// // onCreate 수정: super 호출 후 시스템 스플래시 화면만 사용
-//  override fun onCreate(savedInstanceState: Bundle?) {
-//    super.onCreate(savedInstanceState)
-//
-//    // 시스템에서 제공하는 스플래시 화면만 사용
-////    SplashScreen.installSplashScreen(this)
-////    val splashScreen = installSplashScreen()
-//     installSplashScreen() // SplashScreen 초기화
-//
-//  }
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Android 12 이상에서 시스템 스플래시 화면 사용
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+        }
+
+        // 불필요한 setContentView 호출 생략
+    }
 }
